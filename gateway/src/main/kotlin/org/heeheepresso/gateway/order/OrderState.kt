5e5f9098db83525d9449b6b6eadcf5bed5117553
SeungGun <saverrw2864@naver.com>
2024-02-23 23:53:22 +0900

    feat: implement checking acceptable state by user's role

--- gateway/src/main/kotlin/org/heeheepresso/gateway/order/OrderState.kt
+++ gateway/src/main/kotlin/org/heeheepresso/gateway/order/OrderState.kt
@@ -1,9 +1,26 @@
 package org.heeheepresso.gateway.order
 
+import org.heeheepresso.gateway.user.UserRole
+
 enum class OrderState {
     PAID,
     CHECKED,
     WAITING,
     RECEIVED,
-    CANCELED,
+    CANCELED, // by customer
+    REJECTED; // by employee
+    companion object{
+        fun isAcceptableOrderStateByRole(userRole: UserRole, orderState: OrderState): Boolean{
+            return when(userRole){
+                UserRole.CUSTOMER -> {
+                    when(orderState){
+                        CANCELED -> true
+                        else -> false
+                    }
+                }
+                else -> true
+            }
+        }
+    }
+
 }
