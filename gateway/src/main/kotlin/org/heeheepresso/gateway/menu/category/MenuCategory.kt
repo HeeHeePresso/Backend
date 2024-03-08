296bc4fedccb23bda5a00a14ae71bfb9c0c9eae7
SeungGun <saverrw2864@naver.com>
2024-03-09 01:00:14 +0900

    feat: 메뉴 카테고리 Enum 정의

---
+++ gateway/src/main/kotlin/org/heeheepresso/gateway/menu/category/MenuCategory.kt
@@ -0,0 +1,10 @@
+package org.heeheepresso.gateway.menu.category
+
+enum class MenuCategory {
+    SET_MENU,
+    COFFEE,
+    DECAFFINE_COFFEE,
+    BANACCINO_AND_SMOOTHIE,
+    TEA_AND_ADE,
+    BREAD_AND_DESSERT
+}
