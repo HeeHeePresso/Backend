1da4b9feddcbb6c362c8638c6ed782ca751de1a3
SeungGun <saverrw2864@naver.com>
2024-03-09 01:00:36 +0900

    refactor: 상세 메뉴 데이터에 카테고리 필드 추가

--- gateway/src/main/kotlin/org/heeheepresso/gateway/menu/domain/MenuDetail.kt
+++ gateway/src/main/kotlin/org/heeheepresso/gateway/menu/domain/MenuDetail.kt
@@ -1,11 +1,14 @@
 package org.heeheepresso.gateway.menu.domain
 
+import org.heeheepresso.gateway.menu.category.MenuCategory
+
 data class MenuDetail(
-    val menuId: Long,
-    val imagePath: String,
-    val title: String,
-    val subTitle: String,
-    val price: Long,
+        val menuId: Long,
+        val category: MenuCategory,
+        val imagePath: String,
+        val title: String,
+        val subTitle: String,
+        val price: Long,
 ) {
-    constructor(): this(0, "", "", "", 0)
+    constructor() : this(0, MenuCategory.COFFEE, "", "", "", 0)
 }
