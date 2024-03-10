caf39e456a7d90f6e1835442caaf49ed76059031
SeungGun <saverrw2864@naver.com>
2024-03-10 17:48:46 +0900

    refactor: RecommendedRequest에 메뉴 카테고리 필드(nullable) 추가

--- gateway/src/main/kotlin/org/heeheepresso/gateway/recommendation/RecommendedRequest.kt
+++ gateway/src/main/kotlin/org/heeheepresso/gateway/recommendation/RecommendedRequest.kt
@@ -1,7 +1,10 @@
 package org.heeheepresso.gateway.recommendation
 
+import org.heeheepresso.gateway.menu.category.MenuCategory
+
 data class RecommendedRequest(
     val handler: String,
+    val menuCategory: MenuCategory?,
     val userId: Long,
     val storeId: Long,
     val pageSize: Int,
