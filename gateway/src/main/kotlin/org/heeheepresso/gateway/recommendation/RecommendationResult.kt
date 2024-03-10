50622f5f7824d11ed43e758954135679fc9b7bca
SeungGun <saverrw2864@naver.com>
2024-03-10 17:49:22 +0900

    refactor: 추천 결과에 따른 메뉴 목록 조회 로직에 카테고리 필드 적용

--- gateway/src/main/kotlin/org/heeheepresso/gateway/recommendation/RecommendationResult.kt
+++ gateway/src/main/kotlin/org/heeheepresso/gateway/recommendation/RecommendationResult.kt
@@ -4,16 +4,19 @@ import org.heeheepresso.gateway.menu.domain.Menu
 import org.heeheepresso.gateway.menu.domain.MenuDetail
 
 data class RecommendationResult(
-    val recommendedMenus: List<RecommendedMenu>,
-    val handler: String,
+        val recommendedMenus: List<RecommendedMenu>,
+        val handler: String,
 ) {
     fun getMenus(menuDetails: Map<Long, MenuDetail>): List<Menu> {
         return this.recommendedMenus
-            .map{ menu -> menuDetails.getOrDefault(menu.menuId, MenuDetail()) }
-            .map{ menuDetail -> Menu(
-                imagePath = menuDetail.imagePath,
-                title = menuDetail.title,
-                subTitle = menuDetail.subTitle,
-                price = menuDetail.price) }
+                .map { menu -> menuDetails.getOrDefault(menu.menuId, MenuDetail()) }
+                .map { menuDetail ->
+                    Menu(
+                            menuCategory = menuDetail.category,
+                            imagePath = menuDetail.imagePath,
+                            title = menuDetail.title,
+                            subTitle = menuDetail.subTitle,
+                            price = menuDetail.price)
+                }
     }
 }
