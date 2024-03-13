8748a2de4daaf237c4b7af1bac12ca6ef0ae3ab8
SeungGun <saverrw2864@naver.com>
2024-03-14 00:20:56 +0900

    chore: reactive redis 템플릿 빈 등록

--- gateway/src/main/kotlin/org/heeheepresso/gateway/config/RedisConfig.kt
+++ gateway/src/main/kotlin/org/heeheepresso/gateway/config/RedisConfig.kt
@@ -3,32 +3,24 @@ package org.heeheepresso.gateway.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.redis.connection.RedisConnectionFactory
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
-import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
+import org.springframework.data.redis.core.ReactiveRedisOperations
+import org.springframework.data.redis.core.ReactiveRedisTemplate
+import org.springframework.data.redis.serializer.RedisSerializationContext
 import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
 class RedisConfig {
-    @Value("\${spring.data.redis.host}")
-    lateinit var host: String
-
-    @Value("\${spring.data.redis.port}")
-    lateinit var port: String
-
     @Bean
-    fun redisConnectionFactory(): RedisConnectionFactory {
-        return LettuceConnectionFactory(RedisStandaloneConfiguration(this.host, this.port.toInt()))
-    }
-
-    @Bean
-    fun redisTemplate(): RedisTemplate<String, Any> {
-        val redisTemplate: RedisTemplate<String, Any> = RedisTemplate()
-        redisTemplate.connectionFactory = redisConnectionFactory()
-        redisTemplate.keySerializer = StringRedisSerializer()
-        redisTemplate.valueSerializer = StringRedisSerializer()
-        return redisTemplate
+    fun reactiveRedisTemplate(factory: ReactiveRedisConnectionFactory): ReactiveRedisOperations<String, String> {
+        val serializer = StringRedisSerializer()
+        val serializationContext = RedisSerializationContext.newSerializationContext<String, String>()
+                .key(serializer)
+                .value(serializer)
+                .hashKey(serializer)
+                .hashValue(serializer)
+                .build()
+        return ReactiveRedisTemplate(factory, serializationContext)
     }
 
 }
