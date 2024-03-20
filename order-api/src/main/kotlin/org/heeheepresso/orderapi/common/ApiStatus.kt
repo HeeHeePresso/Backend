15c4611ebb1e47137ec606f4f69869cd83c91b9a
jjh5887 <wjdrnjsgh159@naver.com>
2024-03-20 21:52:14 +0900

    feat: add common exception

---
+++ order-api/src/main/kotlin/org/heeheepresso/orderapi/common/ApiStatus.kt
@@ -0,0 +1,11 @@
+package org.heeheepresso.orderapi.common
+
+enum class ApiStatus(
+    override val code: Int,
+    override val message: String,
+    override val isShort: Boolean,
+    override val isSendErrorMessage: Boolean
+) : ApiStatusResponsible {
+    SUCCESS(0, "성공", false, false),
+    NOT_EXISTS_ORDER_HISTORY(1001, "존재하지 않는 주문 내역입니다.", false, false),
+}
