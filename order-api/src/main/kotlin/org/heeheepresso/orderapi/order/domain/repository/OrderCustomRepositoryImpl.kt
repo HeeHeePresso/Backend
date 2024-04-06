f4a3fd03c7b8d645d4cef7f236a1fb47c99e7cb1
김현교 <zkdlajt96@gmail.com>
2024-04-06 16:20:14 +0900

    fix: delete wrong code

--- order-api/src/main/kotlin/org/heeheepresso/orderapi/order/domain/repository/OrderCustomRepositoryImpl.kt
+++ order-api/src/main/kotlin/org/heeheepresso/orderapi/order/domain/repository/OrderCustomRepositoryImpl.kt
@@ -5,20 +5,13 @@ import org.heeheepresso.orderapi.order.domain.model.Order
 import org.heeheepresso.orderapi.order.domain.model.QOption.option
 import org.heeheepresso.orderapi.order.domain.model.QOrder.*
 import org.heeheepresso.orderapi.order.domain.model.QOrderMenu.orderMenu
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 class OrderCustomRepositoryImpl(
     private val jpaQueryFactory: JPAQueryFactory
 ): OrderCustomRepository {
-    @Query("""
-        SELECT o FROM Order o
-        JOIN FETCH o.orderMenuList om
-        LEFT JOIN FETCH om.options
-        WHERE o.id = :id
-        ORDER BY om.id ASC
-    """)
+
     override fun findByIdJoinAllMenu(id: Long): Order? {
         val query = jpaQueryFactory
             .selectFrom(order)
