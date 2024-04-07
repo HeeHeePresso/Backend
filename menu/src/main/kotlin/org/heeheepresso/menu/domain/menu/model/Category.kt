443932cbbea7a01ab37ce7b8a3473ba8c9eb5732
kkkmj <wjsdus531@naver.com>
2024-04-07 22:42:10 +0900

    [Menu] 메뉴 카테고리 desc 삭제

--- menu/src/main/kotlin/org/heeheepresso/menu/domain/menu/model/Category.kt
+++ menu/src/main/kotlin/org/heeheepresso/menu/domain/menu/model/Category.kt
@@ -1,12 +1,12 @@
 package org.heeheepresso.menu.domain.menu.model
 
-enum class Category (val desc: String) {
-    COFFEE("COFFEE"),
-    DECAFFEINE_COFFEE("DECAFFEINE_COFFEE"),
-    MILK_TEA_AND_LATTE("MILK_TEA_&_LATTE"),
-    JUICE_AND_DRINK("JUICE_&_DRINK"),
-    TEA_AMD_ADE("TEA_&_ADE"),
-    BREAD("BREAD"),
-    DESSERT("DESSERT"),
-    MD("MD"),
+enum class Category() {
+    COFFEE,
+    DECAFFEINE_COFFEE,
+    MILK_TEA_AND_LATTE,
+    JUICE_AND_DRINK,
+    TEA_AMD_ADE,
+    BREAD,
+    DESSERT,
+    MD,
 }
