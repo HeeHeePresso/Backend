fece5e43d6e77c9ae0e2460469f3718e8211890c
kkkmj <wjsdus531@naver.com>
2024-04-07 22:40:39 +0900

    [Menu] 메뉴 api 코드리뷰 반영 - @JpaAuditing 추가

--- menu/src/main/kotlin/org/heeheepresso/menu/MenuApplication.kt
+++ menu/src/main/kotlin/org/heeheepresso/menu/MenuApplication.kt
@@ -2,10 +2,12 @@ package org.heeheepresso.menu
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class MenuApplication
 
 fun main(args: Array<String>) {
-	runApplication<MenuApplication>(*args)
+    runApplication<MenuApplication>(*args)
 }
