307834593aa661c4125f1fbf500c0842eb4be49d
SeungGun <saverrw2864@naver.com>
2024-04-10 17:46:18 +0900

    test: 네이밍 변경

--- gateway/src/test/kotlin/org/heeheepresso/gateway/home/HomeServiceTest.kt
+++ gateway/src/test/kotlin/org/heeheepresso/gateway/home/HomeServiceTest.kt
@@ -4,7 +4,6 @@ import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
-import org.junit.jupiter.api.Assertions.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -19,7 +18,7 @@ class HomeServiceTest(
             Then("여러 서비스로부터 취합한 결과를 반환한다.") {
                 result.eventUrls.size shouldBe 3
                 result.menuInfos[0].menus.size shouldBe 2
-                result.menuInfos[0].title shouldContain "SEASON_RECOMMENDED"
+                result.menuInfos[0].handler shouldContain "SEASON_RECOMMENDED"
                 result.menuInfos[0].menus.stream().map { menu -> menu.name }.toList() shouldContainAll listOf("신승건", "테스트")
             }
         }
