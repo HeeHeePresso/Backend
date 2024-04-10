a0f73d067e37693b089d2065b85533b990dee390
SeungGun <saverrw2864@naver.com>
2024-04-10 17:27:09 +0900

    refactor: 미사용 메소드 삭제

--- gateway/src/main/kotlin/org/heeheepresso/gateway/recommendation/RecommendationHandler.kt
+++ gateway/src/main/kotlin/org/heeheepresso/gateway/recommendation/RecommendationHandler.kt
@@ -6,10 +6,4 @@ enum class RecommendationHandler {
     BREAD,
     SEASON_RECOMMENDED,
     MENU_CATEGORY; // 카테고리에 따른 메뉴 목록 조회에 대한 핸들러
-
-    companion object {
-        fun convertStringsToHandlers(strings: List<String>): List<RecommendationHandler> {
-            return strings.map { valueOf(it) }
-        }
-    }
 }
