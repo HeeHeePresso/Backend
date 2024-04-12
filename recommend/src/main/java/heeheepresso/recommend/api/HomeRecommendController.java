dcbcc813b3bafb05f6bc8f8d2b8f852ac4dc1510
Cho-El <68215871+Cho-El@users.noreply.github.com>
2024-04-12 17:48:26 +0900

    feat: Elastic Search 연동

--- recommend/src/main/java/heeheepresso/recommend/api/HomeRecommendController.java
+++ recommend/src/main/java/heeheepresso/recommend/api/HomeRecommendController.java
@@ -1,4 +1,21 @@
 package heeheepresso.recommend.api;
 
+import heeheepresso.recommend.domain.home.service.SeasonSpecialItemsService;
+import heeheepresso.recommend.global.dto.request.RecommendRequestDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("")
 public class HomeRecommendController {
+    private final SeasonSpecialItemsService seasonSpecialItemsService;
+
+    @PostMapping
+    public ResponseEntity<?> getSeasonSpecialItemList(@RequestBody RecommendRequestDto recommendRequestDto) {
+        return ResponseEntity.ok().build();
+    }
 }
--- recommend/src/main/java/heeheepresso/recommend/global/config/ElasticSearchConfig.java
+++ recommend/src/main/java/heeheepresso/recommend/global/config/ElasticSearchConfig.java
@@ -2,10 +2,13 @@ package heeheepresso.recommend.global.config;
 
 import heeheepresso.recommend.domain.home.repository.SeasonSpecialItemsRepository;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
 @EnableElasticsearchRepositories(basePackageClasses = SeasonSpecialItemsRepository.class)
-public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
+public class ElasticSearchConfig extends ElasticsearchConfiguration {
+    private String username;
+    private String password;
 
 }
