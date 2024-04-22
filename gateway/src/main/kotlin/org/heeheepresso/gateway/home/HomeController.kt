eeca974213ed67e29df4afba1dd6ac83c01c3301
SeungGun <saverrw2864@naver.com>
2024-04-22 11:31:17 +0900

    feat: 홈 화면 api swagger 등록

--- gateway/src/main/kotlin/org/heeheepresso/gateway/home/HomeController.kt
+++ gateway/src/main/kotlin/org/heeheepresso/gateway/home/HomeController.kt
@@ -1,5 +1,11 @@
 package org.heeheepresso.gateway.home
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -9,6 +15,12 @@ class HomeController(
         private val homeService: HomeService
 ) {
 
+    @Operation(summary = "Home Page API")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Success", content = [
+            Content(mediaType = "application/json", array = ArraySchema(schema = Schema(implementation = HomePageResponse::class)))
+        ])
+    ])
     @GetMapping("/home")
     suspend fun getHome(
             @RequestParam("userId") userId: Long
