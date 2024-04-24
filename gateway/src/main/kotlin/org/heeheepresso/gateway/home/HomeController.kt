5b47f606c9b4369b316492163b307427b8fdd926
SeungGun <saverrw2864@naver.com>
2024-04-25 00:09:35 +0900

    refactor: 공통 응답 폼 변경에 따른 응답 타입 변경

--- gateway/src/main/kotlin/org/heeheepresso/gateway/home/HomeController.kt
+++ gateway/src/main/kotlin/org/heeheepresso/gateway/home/HomeController.kt
@@ -1,11 +1,7 @@
 package org.heeheepresso.gateway.home
 
 import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.media.ArraySchema
-import io.swagger.v3.oas.annotations.media.Content
-import io.swagger.v3.oas.annotations.media.Schema
-import io.swagger.v3.oas.annotations.responses.ApiResponse
-import io.swagger.v3.oas.annotations.responses.ApiResponses
+import org.heeheepresso.gateway.common.response.GatewayResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -16,15 +12,10 @@ class HomeController(
 ) {
 
     @Operation(summary = "Home Page API")
-    @ApiResponses(value = [
-        ApiResponse(responseCode = "200", description = "Success", content = [
-            Content(mediaType = "application/json", array = ArraySchema(schema = Schema(implementation = HomePageResponse::class)))
-        ])
-    ])
     @GetMapping("/home")
     suspend fun getHome(
             @RequestParam("userId") userId: Long
-    ): HomePageResponse {
-        return homeService.getHomeData(userId)
+    ): GatewayResponse<HomePageResponse> {
+        return GatewayResponse(homeService.getHomeData(userId))
     }
 }
