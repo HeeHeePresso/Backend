880adeb5d27e30f56496d298305ba3795435f24a
SeungGun <saverrw2864@naver.com>
2024-04-26 14:21:24 +0900

    refactor: 추천 응답에 대한 error 필드 nullable로 수정

--- gateway/src/main/kotlin/org/heeheepresso/gateway/recommendation/client/ErrorResponse.kt
+++ gateway/src/main/kotlin/org/heeheepresso/gateway/recommendation/client/ErrorResponse.kt
@@ -1,9 +1,7 @@
 package org.heeheepresso.gateway.recommendation.client
 
-import org.apache.http.HttpStatus
-
 data class ErrorResponse(
-        val httpStatus: HttpStatus,
+//        val httpStatus: HttpStatus,
         val code: Int,
         val message: String
 )
