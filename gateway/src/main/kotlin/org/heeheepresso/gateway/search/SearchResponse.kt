c5056ce67f0a0baf4d5a5143b9838ce8e5d097c1
SeungGun <saverrw2864@naver.com>
2024-05-31 22:41:00 +0900

    refactor: 검색 타입, 요청 핸들러로 결과의 제네릭 타입으로 최종 결과 반환 메소드 구현

--- gateway/src/main/kotlin/org/heeheepresso/gateway/search/SearchResponse.kt
+++ gateway/src/main/kotlin/org/heeheepresso/gateway/search/SearchResponse.kt
@@ -1,14 +1,24 @@
 package org.heeheepresso.gateway.search
 
+import org.heeheepresso.gateway.search.request.SearchRequestHandler
+import org.heeheepresso.gateway.search.searcher.SearcherType
+
 data class SearchResponse(
-    val results: List<SearchResult>,
-    val extra: MutableMap<String, List<Any>> = HashMap(),
+        val results: List<SearchResult>,
+//    val extra: MutableMap<String, List<Any>> = HashMap(),
 ) {
     companion object {
         const val MENU_DETAIL_IDS = "menuDetailIds"
     }
 
     fun getTotalMenuIds(): List<Long> {
-        return results.mapNotNull { it.menuIds }.flatten()
+        return results.mapNotNull { it.searched }.filterIsInstance<List<Long>>().flatten()
+    }
+
+    inline fun <reified T> getResultBy(searcherType: SearcherType, searchRequestHandler: SearchRequestHandler): List<T> {
+        val result = this.results
+                .filter { it.searcherType == searcherType }
+                .firstOrNull { it.searchRequestHandler == searchRequestHandler }
+        return result?.searched?.filterIsInstance<T>() ?: emptyList()
     }
 }
