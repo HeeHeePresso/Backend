34ea9d01c4a4e9915ac865ac0bc573a45efe4bfb
SeungGun <saverrw2864@naver.com>
2024-05-31 22:34:58 +0900

    feat: 메뉴 카테고리 추천 요청에 대한 uri 추가

--- gateway/src/main/kotlin/org/heeheepresso/gateway/common/extension/StringExtension.kt
+++ gateway/src/main/kotlin/org/heeheepresso/gateway/common/extension/StringExtension.kt
@@ -1,7 +1,7 @@
 package org.heeheepresso.gateway.common.extension
 
 import org.heeheepresso.gateway.menu.category.MenuCategory
-import org.heeheepresso.gateway.search.request.SearchRequestHandler
+import org.heeheepresso.gateway.search.request.SearchRequestHandler.MENU_CATEGORY
 import org.heeheepresso.gateway.search.request.SearchRequestHandler.HOME
 
 class StringExtension {
@@ -15,6 +15,8 @@ internal fun String.getMenuCategory(): MenuCategory {
 internal fun String.convertRecommendedUrl(): String {
     return when(this) {
         HOME.name -> "/home/recommend"
+//        MENU_CATEGORY.name -> "/menu/category/recommend"
+        MENU_CATEGORY.name -> "/home/recommend" // 테스트 때문에 임시로
         else -> ""
     }
 }
