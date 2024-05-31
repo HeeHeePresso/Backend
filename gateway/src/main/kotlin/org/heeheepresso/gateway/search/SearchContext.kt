edba868297dd93d1479229a43bd3cc6d290943a4
SeungGun <saverrw2864@naver.com>
2024-05-31 22:32:48 +0900

    feat: 검색 컨텍스트에 '카테고리' 필드 추가

--- gateway/src/main/kotlin/org/heeheepresso/gateway/search/SearchContext.kt
+++ gateway/src/main/kotlin/org/heeheepresso/gateway/search/SearchContext.kt
@@ -1,15 +1,17 @@
 package org.heeheepresso.gateway.search
 
 import org.heeheepresso.gateway.context.ContextElaborator
+import org.heeheepresso.gateway.menu.category.MenuCategory
 import org.heeheepresso.gateway.processor.post.PostProcessor
 import org.heeheepresso.gateway.search.query.SearchQuery
 
 data class SearchContext(
-    private var userInfo: UserInfo,
-    val searchRequestType: SearchRequestType,
-    val contextElaborators: List<ContextElaborator>,
-    val searchQueries: List<SearchQuery>,
-    val postProcessors: List<PostProcessor>,
+        private var userInfo: UserInfo,
+        val searchRequestType: SearchRequestType,
+        val contextElaborators: List<ContextElaborator>,
+        val searchQueries: List<SearchQuery>,
+        val postProcessors: List<PostProcessor>,
+        val category: MenuCategory? = null
 ) {
     fun decorateUserInfo(userId: Long, storeId: Long) {
         this.userInfo = UserInfo(userId = userId, storeId = storeId)
