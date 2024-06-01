7b513230a6e03b8a76ee27e7b8b59c7fc33ac120
SeungGun <saverrw2864@naver.com>
2024-06-01 18:15:46 +0900

    feat: SearchRequest에 카테고리 추가에 따른 생성자 정의

--- gateway/src/main/kotlin/org/heeheepresso/gateway/search/request/SearchRequest.kt
+++ gateway/src/main/kotlin/org/heeheepresso/gateway/search/request/SearchRequest.kt
@@ -3,13 +3,16 @@ package org.heeheepresso.gateway.search.request
 import org.heeheepresso.gateway.menu.category.MenuCategory
 
 data class SearchRequest(
-    val userId: Long,
-    val handler: SearchRequestHandler,
-    val storeId: Long,
-    val pageSize: Int,
-    val offset: Int,
-    val category: MenuCategory?
+        val userId: Long,
+        val handler: SearchRequestHandler,
+        val storeId: Long,
+        val pageSize: Int,
+        val offset: Int,
+        val category: MenuCategory?
 ) {
-    constructor(userId: Long, handler: SearchRequestHandler, pageSize: Int):
-        this(userId = userId, handler = handler, storeId = 0, pageSize = pageSize, offset = 0, category = null)
+    constructor(userId: Long, handler: SearchRequestHandler, pageSize: Int) :
+            this(userId = userId, handler = handler, storeId = 0, pageSize = pageSize, offset = 0, category = null)
+
+    constructor(userId: Long, handler: SearchRequestHandler, pageSize: Int, category: MenuCategory?) :
+            this(userId = userId, handler = handler, pageSize = pageSize, storeId = 0, offset = 0, category = category)
 }
