f7aab308643a9add5aef4356b0d49936050b15a3
SeungGun <saverrw2864@naver.com>
2024-06-01 18:18:24 +0900

    refactor: 검색 응답 클래스에 extra 필드 제거, 검색 유형에 따른 SearchResult의 실제 결과 조회 메소드 구현

--- gateway/src/main/kotlin/org/heeheepresso/gateway/search/SearchResponse.kt
+++ gateway/src/main/kotlin/org/heeheepresso/gateway/search/SearchResponse.kt
@@ -1,14 +1,21 @@
 package org.heeheepresso.gateway.search
 
+import org.heeheepresso.gateway.search.request.SearchRequestHandler
+import org.heeheepresso.gateway.search.searcher.SearcherType
+
 data class SearchResponse(
-    val results: List<SearchResult>,
-    val extra: MutableMap<String, List<Any>> = HashMap(),
+        val results: List<SearchResult>,
 ) {
-    companion object {
-        const val MENU_DETAIL_IDS = "menuDetailIds"
-    }
 
     fun getTotalMenuIds(): List<Long> {
-        return results.mapNotNull { it.menuIds }.flatten()
+        return results.first { it.searcherType == SearcherType.RECOMMENDATION }
+                .searched?.filterIsInstance<Long>()
+                ?: emptyList()
+    }
+
+    inline fun <reified T> getResultBy(searcherType: SearcherType, searchRequestHandler: SearchRequestHandler): List<T> {
+        val result = this.results.filter { it.searcherType == searcherType }
+                .firstOrNull { it.searchRequestHandler == searchRequestHandler }
+        return result?.searched?.filterIsInstance<T>() ?: emptyList()
     }
 }
