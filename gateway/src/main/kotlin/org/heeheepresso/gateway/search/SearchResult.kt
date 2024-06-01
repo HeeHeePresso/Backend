53198eb4d1ecced1049f4f534f9689a1f066e936
SeungGun <saverrw2864@naver.com>
2024-06-01 18:16:22 +0900

    refactor: 서치 결과에 'searched' Any 타입의 공통 필드 정의

--- gateway/src/main/kotlin/org/heeheepresso/gateway/search/SearchResult.kt
+++ gateway/src/main/kotlin/org/heeheepresso/gateway/search/SearchResult.kt
@@ -8,6 +8,5 @@ data class SearchResult(
     val statusCode: StatusCode,
     val searcherType: SearcherType,
     val searchRequestHandler: SearchRequestHandler,
-    val imageUrls: List<String>? = null,
-    val menuIds: List<Long>? = null,
+    var searched: List<Any>? = null
 )
