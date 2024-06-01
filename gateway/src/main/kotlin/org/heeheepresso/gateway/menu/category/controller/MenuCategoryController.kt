9fe7ccb016864c2a70d45e51bdfb81276cab4949
SeungGun <saverrw2864@naver.com>
2024-06-01 18:38:47 +0900

    feat: 카테고리로 메뉴 목록 조회 API 구현

---
+++ gateway/src/main/kotlin/org/heeheepresso/gateway/menu/category/controller/MenuCategoryController.kt
@@ -0,0 +1,19 @@
+package org.heeheepresso.gateway.menu.category.controller
+
+import org.heeheepresso.gateway.common.response.GatewayResponse
+import org.heeheepresso.gateway.menu.category.CategoryMenusResponse
+import org.heeheepresso.gateway.menu.category.MenuCategory
+import org.heeheepresso.gateway.menu.category.service.MenuCategoryService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class MenuCategoryController(
+        private val menuCategoryService: MenuCategoryService
+) {
+    @GetMapping("/menus")
+    suspend fun getMenusByCategory(@RequestParam("category") menuCategory: MenuCategory, @RequestParam("userId") userId: Long): GatewayResponse<CategoryMenusResponse> {
+        return GatewayResponse(menuCategoryService.getMenusByCategory(menuCategory, userId))
+    }
+}
