1b1732371304696c22476a6d14f79a7bb502aff7
SeungGun <saverrw2864@naver.com>
2024-06-01 18:17:39 +0900

    refactor: 메뉴 상세 프로세서에 상세 메뉴 조회 결과를 검색 결과에 overwrite 처리

--- gateway/src/main/kotlin/org/heeheepresso/gateway/processor/post/MenuDetailSearchProcessor.kt
+++ gateway/src/main/kotlin/org/heeheepresso/gateway/processor/post/MenuDetailSearchProcessor.kt
@@ -3,20 +3,23 @@ package org.heeheepresso.gateway.processor.post
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import org.heeheepresso.gateway.menu.detail.MenuDetailService
+import org.heeheepresso.gateway.menu.domain.MenuBase
 import org.heeheepresso.gateway.search.SearchResponse
-import org.heeheepresso.gateway.search.SearchResponse.Companion.MENU_DETAIL_IDS
+import org.heeheepresso.gateway.search.searcher.SearcherType
 import org.springframework.stereotype.Service
 
 @Service
 class MenuDetailSearchProcessor(
-    private val menuDetailService: MenuDetailService
-): PostProcessor {
+        private val menuDetailService: MenuDetailService
+) : PostProcessor {
 
     override suspend fun process(response: SearchResponse) {
         return coroutineScope {
             val menuDetails = async {
-                menuDetailService.getMenuDetails(response.getTotalMenuIds()) }.await()
-            response.extra[MENU_DETAIL_IDS] = menuDetails
+                menuDetailService.getMenuDetails(response.getTotalMenuIds())
+            }.await()
+            response.results.first { it.searcherType == SearcherType.RECOMMENDATION }
+                    .searched = menuDetails.map { MenuBase(it.id, it.name, it.price, it.thumbnailImageUrl) }
         }
     }
 }
